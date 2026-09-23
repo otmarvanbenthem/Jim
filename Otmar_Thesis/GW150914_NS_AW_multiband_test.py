e9d9d76916084d364ba658c88e6414fc1afd57d5
otmarvanbenthem <otmarvanbenthem@gmail.com>
2026-09-23 16:01:14 +0200

    Update GW150914_NS_AW_multiband_test.py

--- Otmar_Thesis/GW150914_NS_AW_multiband_test.py
+++ Otmar_Thesis/GW150914_NS_AW_multiband_test.py
@@ -279,8 +279,8 @@ parameter_labels = {
     "psi": r"$\psi$",
     "ra": r"$\alpha$",
     "dec": r"$\delta$",
-    "lambda_1": r"$\Lambda_1$",
-    "lambda_2": r"$\Lambda_2$",
+   # "lambda_1": r"$\Lambda_1$",
+    #"lambda_2": r"$\Lambda_2$",
 }
 #%%
 print("plotting")
