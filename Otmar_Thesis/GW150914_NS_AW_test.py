bf94296c9e68dbe96c3bfad49f83a7ceafff24fb
otmarvanbenthem <otmarvanbenthem@gmail.com>
2026-09-25 16:49:09 +0200

    hi

--- Otmar_Thesis/GW150914_NS_AW_test.py
+++ Otmar_Thesis/GW150914_NS_AW_test.py
@@ -282,5 +282,6 @@ parameter_labels = {
 fig = corner.corner(
     np.stack([chains[key] for key in jim.prior.parameter_names]).T,
     labels=[parameter_labels.get(k, k) for k in jim.prior.parameter_names],
+    truths = [injection_parameters[k] for k in jim.prior.parameter_names],
 )
 fig.savefig(Path(__file__).parent / "GW150914_NS_AW_OTMAR.png")
--- Otmar_Thesis/GW150914_test_multiband.py
+++ Otmar_Thesis/GW150914_test_multiband.py
@@ -198,5 +198,6 @@ parameter_labels = {
 fig = corner.corner(
     np.stack([chains[key] for key in jim.prior.parameter_names]).T[::10],
     labels=[parameter_labels.get(k, k) for k in jim.prior.parameter_names],
+     truths = [injection_parameters[k] for k in jim.prior.parameter_names],
 )
 fig.savefig(Path(__file__).parent / "GW150914_multiband_Otmar.png")
